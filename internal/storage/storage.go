9efa3e40e08865e31f9527eb919482e3b3cfcd68
assilbekov <askhat.assilbekov@gmail.com>
2024-05-25 17:07:35 -0300

    storage

---
+++ internal/storage/storage.go
@@ -0,0 +1,54 @@
+package storage
+
+import "sync"
+
+type Line struct {
+	Soccer   float64
+	Football float64
+	Baseball float64
+}
+
+type Storage struct {
+	mu    sync.RWMutex
+	lines Line
+
+	// Is there a better way to do this?
+	synced bool
+}
+
+func NewStorage() *Storage {
+	return &Storage{}
+}
+
+func (s *Storage) UpdateLines(newLines Line) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if newLines.Soccer != 0 {
+		s.lines.Soccer = newLines.Soccer
+	}
+	if newLines.Football != 0 {
+		s.lines.Football = newLines.Football
+	}
+	if newLines.Baseball != 0 {
+		s.lines.Baseball = newLines.Baseball
+	}
+
+	s.synced = true
+}
+
+func (s *Storage) GetLines() Line {
+	// Is this going to block the execution thread?
+	// Will I be able to read the lines while they are being updated?
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return s.lines
+}
+
+func (s *Storage) IsSynced() bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return s.synced
+}
