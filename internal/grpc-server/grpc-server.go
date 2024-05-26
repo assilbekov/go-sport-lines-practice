0fe7b60a6bf31f843f89e124b9a8942217ca7141
assilbekov <askhat.assilbekov@gmail.com>
2024-05-26 20:49:23 -0300

    confusing pause of grpc

--- internal/grpc-server/grpc-server.go
+++ internal/grpc-server/grpc-server.go
@@ -1,7 +1,39 @@
 package grpcserver
 
-import "go-sport-lines-practice/api/proto/pkg/lines"
+import (
+	"go-sport-lines-practice/api/proto/pkg/lines"
+	"go-sport-lines-practice/internal/storage"
+	"log/slog"
+	"net"
+	"sync"
+)
 
 type Server struct {
 	lines.UnimplementedLinesServiceServer
+	store  *storage.Storage
+	synced bool
+	mu     sync.Mutex
+	subs   map[lines.LinesService_SubscribeOnSportLinesClient][]string
+}
+
+func NewServer(store *storage.Storage) *Server {
+	return &Server{
+		store: store,
+		subs:  make(map[lines.LinesService_SubscribeOnSportLinesClient][]string),
+	}
+}
+
+func (s *Server) Start(addr string) {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		slog.Error("failed to listen", "err", err)
+		return
+	}
+
+	grpcServer := lines.LinesServiceServer(s)
+	lines.RegisterLinesServiceServer(grpcServer, s)
+	slog.Info("grpc server started", "addr", addr)
+	if err = grpcServer.Serve(lis); err != nil {
+		slog.Error("failed to serve", "err", err)
+	}
 }
